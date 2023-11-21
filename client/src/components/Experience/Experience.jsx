cac83bb43639471345f71bc401e4e0edd18c6920
Harris Greenstein <harrisgreenstein@gmail.com>
2023-11-21 02:05:03 -0500

    Fixed Experience Layout

--- client/src/components/Experience/Experience.jsx
+++ client/src/components/Experience/Experience.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './experience.css';
 import Carousel from 'react-bootstrap/Carousel';
 import ExperienceButton from './ExperienceButton';
-import {ExperienceAspirisObject, ExperienceCatchpointObject} from './ExperienceSwipeObject.jsx';
-import CatchpointLogo from './assets/catchpointLogo.png';
+import {
+    ExperienceAspirisObject,
+    ExperienceCatchpointObject,
+} from './ExperienceSwipeObject.jsx';
+import CatchpointLogo from './assets/catchpointLogoCropped.png';
 const ExperiencePage = () => {
     return (
         <>
@@ -11,6 +14,18 @@ const ExperiencePage = () => {
                 <div className="experience-title">
                     <h1>My Experience</h1>
                 </div>
+                <div className="experience-display-content">
+                    <div className="experience-buttons">
+                        <ExperienceButton Image={CatchpointLogo} />
+                        <ExperienceButton
+                            Image={
+                                'https://aspiris.com/wp-content/uploads/2020/07/Aspiris-logo.png'
+                            }
+                        />
+                        <ExperienceButton />
+                    </div>
+                </div>
+
                 <div className="experience-content-grid">
                     {/* <div class="experience-swiper-container">{items}</div> */}
                     <Carousel data-bs-theme="dark" auto={false}>
@@ -24,14 +39,6 @@ const ExperiencePage = () => {
                             <ExperienceCatchpointObject />
                         </Carousel.Item>
                     </Carousel>
-
-                    <div className="experience-display-content">
-                        <div className="experience-buttons">
-                            <ExperienceButton Image={CatchpointLogo}/>
-                            <ExperienceButton Image={"https://aspiris.com/wp-content/uploads/2020/07/Aspiris-logo.png"}/>
-                            <ExperienceButton />
-                        </div>
-                    </div>
                 </div>
             </div>
         </>
--- client/src/components/Experience/ExperienceSwipeObject.jsx
+++ client/src/components/Experience/ExperienceSwipeObject.jsx
@@ -17,8 +17,11 @@ function ExperienceCatchpointObject() {
                 </div>
                 <ul className="experience-object-list">
                     <li>
-                        Led the creation of a <a href="https://www.catchpoint.com/workforce-experience/zoom">VoIP & video feature</a> for proactive
-                        outage alerts and enhanced call quality
+                        Led the creation of a{' '}
+                        <a href="https://www.catchpoint.com/workforce-experience/zoom">
+                            VoIP & video feature
+                        </a>{' '}
+                        for proactive outage alerts and enhanced call quality
                     </li>
                     <li>
                         Managed product development through agile methodologies,
@@ -31,7 +34,22 @@ function ExperienceCatchpointObject() {
                         departments
                     </li>
                 </ul>
-                <h4 className="experience-object-quote">                    <i>"Through meetings with our customer success, sales, and engineering teams, and hearing feedback from customers who requested this feature, I was able to give a much more refined problem and solution definition that I knew better aligned with the potential future users of this feature...The highlight of my time here was getting to work with so many amazing teams of people from all areas of the company to develop such a feature." <a href="https://www.catchpoint.com/blog/celebrating-the-amazing-work-of-our-2021-interns"> - 2021 Catchpoint Intern Blog </a></i>
+                <h4 className="experience-object-quote">
+                    {' '}
+                    <i>
+                        "Through meetings with our customer success, sales, and
+                        engineering teams, and hearing feedback from customers
+                        who requested this feature, I was able to give a much
+                        more refined problem and solution definition that I knew
+                        better aligned with the potential future users of this
+                        feature...The highlight of my time here was getting to
+                        work with so many amazing teams of people from all areas
+                        of the company to develop such a feature."{' '}
+                        <a href="https://www.catchpoint.com/blog/celebrating-the-amazing-work-of-our-2021-interns">
+                            {' '}
+                            - 2021 Catchpoint Intern Blog{' '}
+                        </a>
+                    </i>
                 </h4>
             </div>
         </>
@@ -53,17 +71,39 @@ function ExperienceAspirisObject() {
                     />
                 </div>
                 <ul className="experience-object-list">
-                    <li>Engineered a solution using RESTful APIs to consolidate data across platforms for accurate billing and cost savings</li>
                     <li>
-                       Provided on-site tech support and repairs on PCs, servers, networking equipment, printers, and software upgrades                    </li>
+                        Engineered a solution using RESTful APIs to consolidate
+                        data across platforms for accurate billing and cost
+                        savings
+                    </li>
                     <li>
-                        Led trial and testing of Security Information and Event Management dashboard and alert package                    </li>
+                        Provided on-site tech support and repairs on PCs,
+                        servers, networking equipment, printers, and software
+                        upgrades{' '}
+                    </li>
+                    <li>
+                        Led trial and testing of Security Information and Event
+                        Management dashboard and alert package{' '}
+                    </li>
                 </ul>
                 <h4 className="experience-object-quote">
-                    <i>"Through meetings with our customer success, sales, and engineering teams, and hearing feedback from customers who requested this feature, I was able to give a much more refined problem and solution definition that I knew better aligned with the potential future users of this feature...The highlight of my time here was getting to work with so many amazing teams of people from all areas of the company to develop such a feature." <a href="https://www.catchpoint.com/blog/celebrating-the-amazing-work-of-our-2021-interns"> - 2021 Catchpoint Intern Blog </a></i>
+                    <i>
+                        "Through meetings with our customer success, sales, and
+                        engineering teams, and hearing feedback from customers
+                        who requested this feature, I was able to give a much
+                        more refined problem and solution definition that I knew
+                        better aligned with the potential future users of this
+                        feature...The highlight of my time here was getting to
+                        work with so many amazing teams of people from all areas
+                        of the company to develop such a feature."{' '}
+                        <a href="https://www.catchpoint.com/blog/celebrating-the-amazing-work-of-our-2021-interns">
+                            {' '}
+                            - 2021 Catchpoint Intern Blog{' '}
+                        </a>
+                    </i>
                 </h4>
             </div>
         </>
     );
 }
-export {ExperienceAspirisObject, ExperienceCatchpointObject};
+export { ExperienceAspirisObject, ExperienceCatchpointObject };
