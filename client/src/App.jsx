36b8ea5746344096acbae606fed20789c668d1ca
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-05 03:57:58 +0000

    Fixed Google Analytics Environment Variable
    Expects a local .env file in the client folder with the site ID

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         const theme = darkMode ? 'dark' : 'light';
         document.documentElement.setAttribute('data-bs-theme', theme);
     }, [darkMode]);
-    const googleAnalyticsId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+    const googleAnalyticsId = import.meta.env.VITE_GA_ID;
     if (googleAnalyticsId) {
         ReactGA.initialize(googleAnalyticsId);
         ReactGA.pageview(window.location.pathname + window.location.search);
