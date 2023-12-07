502934f5f31201ff3b7a8127c0fa246f85b5b6bb
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-06 20:22:55 -0500

    Removed conflicting loading ring styles -Duplicate classes in index.css and index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
                                 darkMode={darkMode}
                                 setDarkMode={setDarkMode}
                             />{' '}
-                            <About key={darkMode} darkMode={darkMode} />
+                            <AboutDev key={darkMode} darkMode={darkMode} />
                             <Experience />
                             <Projects />
                             <Contact />
