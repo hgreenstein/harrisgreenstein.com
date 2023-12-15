2487d17e306d7e2dc1d0ea6cf82b84b2898e9598
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-14 21:39:33 -0500

    Adding Landing Page and Styling for Blog - Available at /blog

---
+++ client/src/components/Blog/Landing.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './blog.scss';
+import BlogArticleItem from './BlogArticleCard.jsx';
+import DalleCoverPhoto from './assets/dalleCoverPhoto.png';
+function Landing() {
+    const articles = [
+        {
+            title: 'How Coders Will Co-Program With A.I.',
+            summary: "   This article explores the future of programming alongside A.I., emphasizing a collaborative approach where coders and artificial intelligence systems enhance each other's capabilities. It introduces the innovative \“//py\” mode, designed for seamless integration of natural language and coding, and discusses the transformative impact of Nova Mode on ChatGPT's functionality. The piece highlights how these advancements will enable programmers to express complex coding logic in simpler terms, revolutionizing the way we interact with coding languages. ",
+            image: DalleCoverPhoto,
+            link: "/blog/how_will_coders_co_program_with_ai",
+        },
+    ];
+    return (
+        <section id="landing">
+            <h1 className="blog-title">
+                Blog
+            </h1>
+            {articles.map((article, index) => (
+                <BlogArticleItem
+                    key={index}
+                    title={article.title}
+                    summary={article.summary}
+                    image={article.image}
+                    link={article.link}
+                />
+            ))}
+        </section>
+    );
+}
+export default Landing;
