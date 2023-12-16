5bd21a3ea83647a62d3ef8a2ca5199c276dfc304
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-16 18:04:07 -0500

    Changing all instances of "A.I." to "AI" for consistency
    Changing all instances of "A.I." to "AI" for consistency

--- client/src/components/Blog/Landing.jsx
+++ client/src/components/Blog/Landing.jsx
@@ -5,8 +5,8 @@ import DalleCoverPhoto from './assets/dalleCoverPhoto.png';
 function Landing() {
     const articles = [
         {
-            title: 'How Coders Will Co-Program With A.I.',
-            summary: "   This article explores the future of programming alongside A.I., emphasizing a collaborative approach where coders and artificial intelligence systems enhance each other's capabilities. It introduces the innovative \“//py\” mode, designed for seamless integration of natural language and coding, and discusses the transformative impact of Nova Mode on ChatGPT's functionality. The piece highlights how these advancements will enable programmers to express complex coding logic in simpler terms, revolutionizing the way we interact with coding languages. ",
+            title: 'How Coders Will Co-Program With AI',
+            summary: "   This article explores the future of programming alongside AI, emphasizing a collaborative approach where coders and artificial intelligence systems enhance each other's capabilities. It introduces the innovative \“//py\” mode, designed for seamless integration of natural language and coding, and discusses the transformative impact of Nova Mode on ChatGPT's functionality. The piece highlights how these advancements will enable programmers to express complex coding logic in simpler terms, revolutionizing the way we interact with coding languages. ",
             image: DalleCoverPhoto,
             link: "/blog/how_will_coders_co_program_with_ai",
         },
